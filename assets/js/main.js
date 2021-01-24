76445e36b8c25afc42819404d0adb4730d777fcb
wookey <anbriaao@naver.com>
2021-01-24 17:15:35 +0900

    hf

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,250 +4,259 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
-
-	var	$window = $(window),
-		$body = $('body');
-
-	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ null,      '480px'  ]
-		});
+(function ($) {
+
+    var $window = $(window),
+        $body = $('body');
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+    // Breakpoints.
+    breakpoints({
+        xlarge: ['1281px', '1680px'],
+        large: ['981px', '1280px'],
+        medium: ['737px', '980px'],
+        small: ['481px', '736px'],
+        xsmall: [null, '480px']
+    });
 
-	// Touch mode.
-		if (browser.mobile)
-			$body.addClass('is-touch');
+    // Play initial animations on page load.
+    $window.on('load', function () {
+        window.setTimeout(function () {
+            $body.removeClass('is-preload');
+        }, 100);
+    });
 
-	// Scrolly links.
-		$('.scrolly').scrolly({
-			speed: 2000
-		});
+    // Touch mode.
+    if (browser.mobile)
+        $body.addClass('is-touch');
 
-	// Dropdowns.
-		$('#nav > ul').dropotron({
-			alignment: 'right',
-			hideDelay: 350
-		});
+    // Scrolly links.
+    $('.scrolly').scrolly({
+        speed: 2000
+    });
 
-	// Nav.
+    // Dropdowns.
+    $('#nav > ul').dropotron({
+        alignment: 'right',
+        hideDelay: 350
+    });
 
-		// Title Bar.
-			$(
-				'<div id="titleBar">' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
-				'</div>'
-			)
-				.appendTo($body);
+    // Nav.
 
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
+    // Title Bar.
+    $(
+        '<div id="titleBar">' +
+        '<span class="title">' + $('#logo').html() + '</span>' +
+        '</div>'
+    )
+        .appendTo($body);
 
-	// Parallax.
-	// Disabled on IE (choppy scrolling) and mobile platforms (poor performance).
-		if (browser.name == 'ie'
-		||	browser.mobile) {
+    // Panel.
+    $(
+        '<div id="navPanel">' +
+        '<nav>' +
+        $('#nav').navList() +
+        '</nav>' +
+        '</div>'
+    )
+        .appendTo($body)
+        .panel({
+            delay: 500,
+            hideOnClick: true,
+            hideOnSwipe: true,
+            resetScroll: true,
+            resetForms: true,
+            side: 'left',
+            target: $body,
+            visibleClass: 'navPanel-visible'
+        });
 
-			$.fn._parallax = function() {
+    // Parallax.
+    // Disabled on IE (choppy scrolling) and mobile platforms (poor performance).
+    if (browser.name == 'ie'
+        || browser.mobile) {
 
-				return $(this);
+        $.fn._parallax = function () {
 
-			};
+            return $(this);
 
-		}
-		else {
+        };
 
-			$.fn._parallax = function() {
+    } else {
 
-				$(this).each(function() {
+        $.fn._parallax = function () {
 
-					var $this = $(this),
-						on, off;
+            $(this).each(function () {
 
-					on = function() {
+                var $this = $(this),
+                    on, off;
 
-						$this
-							.css('background-position', 'center 0px');
+                on = function () {
 
-						$window
-							.on('scroll._parallax', function() {
+                    $this
+                        .css('background-position', 'center 0px');
 
-								var pos = parseInt($window.scrollTop()) - parseInt($this.position().top);
+                    $window
+                        .on('scroll._parallax', function () {
 
-								$this.css('background-position', 'center ' + (pos * -0.15) + 'px');
+                            var pos = parseInt($window.scrollTop()) - parseInt($this.position().top);
 
-							});
+                            $this.css('background-position', 'center ' + (pos * -0.15) + 'px');
 
-					};
+                        });
 
-					off = function() {
+                };
 
-						$this
-							.css('background-position', '');
+                off = function () {
 
-						$window
-							.off('scroll._parallax');
+                    $this
+                        .css('background-position', '');
 
-					};
+                    $window
+                        .off('scroll._parallax');
 
-					breakpoints.on('<=medium', off);
-					breakpoints.on('>medium', on);
+                };
 
-				});
+                breakpoints.on('<=medium', off);
+                breakpoints.on('>medium', on);
 
-				return $(this);
+            });
 
-			};
+            return $(this);
 
-			$window
-				.on('load resize', function() {
-					$window.trigger('scroll');
-				});
+        };
 
-		}
+        $window
+            .on('load resize', function () {
+                $window.trigger('scroll');
+            });
 
-	// Spotlights.
-		var $spotlights = $('.spotlight');
+    }
 
-		$spotlights
-			._parallax()
-			.each(function() {
+    // Spotlights.
+    var $spotlights = $('.spotlight');
 
-				var $this = $(this),
-					on, off;
+    $spotlights
+        ._parallax()
+        .each(function () {
 
-				on = function() {
+            var $this = $(this),
+                on, off;
 
-					var top, bottom, mode;
+            on = function () {
 
-					// Use main <img>'s src as this spotlight's background.
-						$this.css('background-image', 'url("' + $this.find('.image.main > img').attr('src') + '")');
+                var top, bottom, mode;
 
-					// Side-specific scrollex tweaks.
-						if ($this.hasClass('top')) {
+                // Use main <img>'s src as this spotlight's background.
+                $this.css('background-image', 'url("' + $this.find('.image.main > img').attr('src') + '")');
 
-							mode = 'top';
-							top = '-20%';
-							bottom = 0;
+                // Side-specific scrollex tweaks.
+                if ($this.hasClass('top')) {
 
-						}
-						else if ($this.hasClass('bottom')) {
+                    mode = 'top';
+                    top = '-20%';
+                    bottom = 0;
 
-							mode = 'bottom-only';
-							top = 0;
-							bottom = '20%';
+                } else if ($this.hasClass('bottom')) {
 
-						}
-						else {
+                    mode = 'bottom-only';
+                    top = 0;
+                    bottom = '20%';
 
-							mode = 'middle';
-							top = 0;
-							bottom = 0;
+                } else {
 
-						}
+                    mode = 'middle';
+                    top = 0;
+                    bottom = 0;
 
-					// Add scrollex.
-						$this.scrollex({
-							mode:		mode,
-							top:		top,
-							bottom:		bottom,
-							initialize:	function(t) { $this.addClass('inactive'); },
-							terminate:	function(t) { $this.removeClass('inactive'); },
-							enter:		function(t) { $this.removeClass('inactive'); },
+                }
 
-							// Uncomment the line below to "rewind" when this spotlight scrolls out of view.
+                // Add scrollex.
+                $this.scrollex({
+                    mode: mode,
+                    top: top,
+                    bottom: bottom,
+                    initialize: function (t) {
+                        $this.addClass('inactive');
+                    },
+                    terminate: function (t) {
+                        $this.removeClass('inactive');
+                    },
+                    enter: function (t) {
+                        $this.removeClass('inactive');
+                    },
 
-							//leave:	function(t) { $this.addClass('inactive'); },
+                    // Uncomment the line below to "rewind" when this spotlight scrolls out of view.
 
-						});
+                    //leave:	function(t) { $this.addClass('inactive'); },
 
-				};
+                });
 
-				off = function() {
+            };
 
-					// Clear spotlight's background.
-						$this.css('background-image', '');
+            off = function () {
 
-					// Remove scrollex.
-						$this.unscrollex();
+                // Clear spotlight's background.
+                $this.css('background-image', '');
 
-				};
+                // Remove scrollex.
+                $this.unscrollex();
 
-				breakpoints.on('<=medium', off);
-				breakpoints.on('>medium', on);
+            };
 
-			});
+            breakpoints.on('<=medium', off);
+            breakpoints.on('>medium', on);
 
-	// Wrappers.
-		var $wrappers = $('.wrapper');
+        });
 
-		$wrappers
-			.each(function() {
+    // Wrappers.
+    var $wrappers = $('.wrapper');
 
-				var $this = $(this),
-					on, off;
+    $wrappers
+        .each(function () {
 
-				on = function() {
+            var $this = $(this),
+                on, off;
 
-					$this.scrollex({
-						top:		250,
-						bottom:		0,
-						initialize:	function(t) { $this.addClass('inactive'); },
-						terminate:	function(t) { $this.removeClass('inactive'); },
-						enter:		function(t) { $this.removeClass('inactive'); },
+            on = function () {
 
-						// Uncomment the line below to "rewind" when this wrapper scrolls out of view.
+                $this.scrollex({
+                    top: 250,
+                    bottom: 0,
+                    initialize: function (t) {
+                        $this.addClass('inactive');
+                    },
+                    terminate: function (t) {
+                        $this.removeClass('inactive');
+                    },
+                    enter: function (t) {
+                        $this.removeClass('inactive');
+                    },
 
-						//leave:	function(t) { $this.addClass('inactive'); },
+                    // Uncomment the line below to "rewind" when this wrapper scrolls out of view.
 
-					});
+                    //leave:	function(t) { $this.addClass('inactive'); },
 
-				};
+                });
 
-				off = function() {
-					$this.unscrollex();
-				};
+            };
 
-				breakpoints.on('<=medium', off);
-				breakpoints.on('>medium', on);
+            off = function () {
+                $this.unscrollex();
+            };
 
-			});
+            breakpoints.on('<=medium', off);
+            breakpoints.on('>medium', on);
 
-	// Banner.
-		var $banner = $('#banner');
+        });
 
-		$banner
-			._parallax();
+    // Banner.
+    var $banner = $('#banner');
+
+    $banner
+        ._parallax();
 
     var typeString = ['Attractive and Fascinating Developer Wookey Portfolio'];
-    var  i = 0;
+    var i = 0;
     var count = 0
     var selectedText = '';
     var text = '';
